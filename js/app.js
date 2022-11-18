2faaf12ce02460df08c1d95e2fafbb5ca6fac48e
pytherik <erikberndt@gmx.net>
2022-11-18 08:07:10 +0100

    calc next nw

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 $("#calc-form").submit((e) => {
   e.preventDefault();
   const octals = {
-    1: $("#one").val(),
-    2: $("#two").val(),
-    3: $("#three").val(),
-    4: $("#four").val()
+    1: parseInt($("#one").val()),
+    2: parseInt($("#two").val()),
+    3: parseInt($("#three").val()),
+    4: parseInt($("#four").val())
   }
   const cidr = $("#cidr").val();
+  let a = Math.floor(cidr /8) 
   const result = getNW(octals, cidr);
-  $(".results").append(`<h1>${result[0][0]}.${result[0][1]}.${result[0][2]}.${result[0][3]}`)
-  $(".results").append(`<h1>${result[1][0]}.${result[1][1]}.${result[1][2]}.${result[1][3]}`)
+  const nw = result[0];
+  const bc = result[1];
+  $(".results").append(`<h1>Netzwerkadresse: ${nw[0]}.${nw[1]}.${nw[2]}.${nw[3]}`)
+  $(".results").append(`<h1>Broadcastadress: ${bc[0]}.${bc[1]}.${bc[2]}.${bc[3]}`)
+  
+  const nx = nw;
+  
+  if (bc[a] != 255) {
+    nx[a] = bc[a] + 1;
+    for (let i = a + 1; i < 4; i++) {
+      nx[i] = 0;
+    }
+  } else {
+    nx[a - 1] = bc[a - 1] + 1;
+    for (let i = a; i < 4; i++) {
+      nx[i] = 0;
+    }
+  }
+  $(".results").append(`<hr><h1>Nächstes Netz : ${nx[0]}.${nx[1]}.${nx[2]}.${nx[3]}`)
 })
